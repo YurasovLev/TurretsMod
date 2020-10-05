8505b804d45c3c9193a1f136e3de5ebdd55902da
lev43 <68313120+lev43@users.noreply.github.com>
2020-10-05 10:35:07 +0300

    f

--- scripts/hurricane.js
+++ scripts/hurricane.js
@@ -1,7 +1,7 @@
 const hurricane = extendContent(ItemTurret, "hurricane", {
 	turnToTarget(tile, targetRot){
 		entity=tile.ent();
-		entity.rotation+=0.5;
+		entity.rotation+=1;
 	},
 	bullet(tile, type, angle){
 		Bullet.create(type, tile.entity, tile.getTeam(), tile.drawx(), tile.drawy(), angle);
