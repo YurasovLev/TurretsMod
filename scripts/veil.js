420ab325cb880e343109276f11b9f2c880cad611
lev43 <68313120+lev43@users.noreply.github.com>
2020-10-08 14:03:24 +0300

    abc

--- scripts/veil.js
+++ scripts/veil.js
@@ -1,11 +1,12 @@
 const veil = extendContent(ItemTurret, "veil", {
-	turnToTarget(tile, targetRot){
-		entity=tile.ent();
-		entity.rotation+=4;
-	},
+	turnToTarget(tile, targetRot){},
 	bullet(tile, type, angle){
 		var drag=Math.random()+0.01;
 		type.drag=(drag>0.5?drag/10:drag);
-		Bullet.create(type, tile.entity, tile.getTeam(), tile.drawx(), tile.drawy(), angle);
+		entity=tile.ent();
+		for(var i=0; i<10; i++){
+			entity.rotation+=4;
+			Bullet.create(type, tile.entity, tile.getTeam(), tile.drawx(), tile.drawy(), angle);
+		}
 	}
 });
