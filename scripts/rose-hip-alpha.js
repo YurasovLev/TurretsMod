42cd61d1b46632c7e577cc1a4f3ee6a508725d3a
lev43 <68313120+lev43@users.noreply.github.com>
2020-11-10 21:07:48 +0300

    Update to 3.0

--- scripts/rose-hip-alpha.js
+++
@@ -1,5 +0,0 @@
-const RoseHipAlpha=extendContent(ItemTurret, "rose-hip-alpha", {
-	bullet(tile, type, angle){
-		Bullet.create(type, tile.entity, tile.getTeam(), tile.drawx(), tile.drawy(), angle);
-	}
-});
